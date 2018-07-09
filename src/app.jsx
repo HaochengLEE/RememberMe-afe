8d9617a54b574c3e6940fef13141712917035404
lihaocheng <lihaocheng00@126.com>
2018-07-09 22:27:55 +0800

    webpackinit

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+/*
+* @Author: lihaocheng
+* @Date:   2018-07-09 17:59:02
+* @Last Modified by:   lihaocheng
+* @Last Modified time: 2018-07-09 20:58:03
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+	<h1>Hello,world</h1>,
+	document.getElementById('app')	
+);
