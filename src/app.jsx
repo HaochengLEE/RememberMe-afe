9c2ed3cde331db7c1eab2b1d486d3ddfaf343d7f
lihaocheng <lihaocheng00@126.com>
2018-07-14 18:37:40 +0800

    zero

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './index.css';
 ReactDOM.render(
 	<h1>Hello,world</h1>,
 	document.getElementById('app')	
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,13 @@ module.exports = {
 	          presets: ['env','react']
 	        }
 	      }
+	    },
+	    {
+	    	test:/\.css$/,
+	    	use:[
+	    		'style-loader',
+	    		'css-loader'
+	    	]
 	    }
 	  ]
 	},
